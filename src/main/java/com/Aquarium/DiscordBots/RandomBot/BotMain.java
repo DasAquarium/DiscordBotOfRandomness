8c55532fc5ae485f2a9f5b1145dd3edda49e98ad
Menkalian <kilian.krampf@outlook.de>
2017-06-21 21:19:15 +0200

    Setting the Project up
    The Initiation of the Project with Gradle

---
+++ src/main/java/com/Aquarium/DiscordBots/RandomBot/BotMain.java
@@ -0,0 +1,23 @@
+package com.Aquarium.DiscordBots.RandomBot;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+
+/**
+ * BotOfRandomness was created by Menkalian in Java
+ */
+public class BotMain extends ListenerAdapter {
+
+    public static void main(String[] args){
+        try {
+            JDA bot = new JDABuilder(AccountType.BOT).setToken("Your Token here").addEventListener(new BotMain()).buildBlocking();
+        } catch (LoginException | InterruptedException | RateLimitedException e) {
+            e.printStackTrace();
+        }
+    }
+}
